68302a04e15bca368243bceed3998141099c436e
Fernando Herrero <herrerofernando.94@gmail.com>
2025-07-01 13:40:50 +0200

    component WelcomeMessage done

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css";
 import { UserCard } from "./components/UserCard/UserCard.jsx";
 import helaImage from "./assets/hela.webp";
 import spiderImage from "./assets/spider-man.jpg";
+import { WelcomeMessage } from "./components/WelcomeMesssage/WelcomeMessage.jsx";
 
 export const App = () => {
 	return (
 		<>
 			<UserCard name="Fer" age={30} city="Madrid" image={spiderImage} nameAvatar={"Fer"} buttonText="Click me!" />
+			<WelcomeMessage name="Fer" />
+
 			<UserCard
 				name="Paola"
 				age={36}
@@ -15,6 +18,7 @@ export const App = () => {
 				nameAvatar={"Paola"}
 				buttonText="Send me!"
 			/>
+			<WelcomeMessage name="Paola" />
 		</>
 	);
 };
